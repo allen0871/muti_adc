3c1ef217821eefdc8a15af0ad96ca777ace1dfc8
allen0871 <allen0871@gmail.com>
2022-07-10 18:33:23 +0800

    改为50k调制

--- Core/Src/adc_config.h
+++ Core/Src/adc_config.h
@@ -6,13 +6,13 @@
 #define NPLCCT (400*NPLC)
 #define RUNDOWN 40
 
-#define TZBASE  250
+#define TZBASE  500
 #define TZCLK  (TZBASE-1)
 #define TZS  10
 #define TZW  TZBASE-TZS
 #define TZCC TZBASE-120
 
-#define ADH  2400
-#define ADL  1200
+#define ADH  2600
+#define ADL  800
 
 #endif
